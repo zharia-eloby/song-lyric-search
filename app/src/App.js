66b1b83920dd11c483d15f6db9d1c225a6cf1189
zharia-eloby <zeloby22@gmail.com>
2025-03-24 22:31:33 -0500

    moved css files into styles folder

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Lyrics from './pages/Lyrics';
