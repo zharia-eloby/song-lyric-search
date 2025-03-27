7c67d31232533455aedbeea65be61a1a9fc4eb27
zharia-eloby <zeloby22@gmail.com>
2025-03-26 22:15:08 -0500

    updated styling

--- app/src/pages/Lyrics.js
+++ app/src/pages/Lyrics.js
@@ -1,5 +1,10 @@
 import { useLocation, Navigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import _ from 'lodash';
+
+function toTitleCase(str) {
+    return str.split(' ').map((word) => _.capitalize(word.toLowerCase())).join(' ');
+}
 
 function Lyrics() {
     const location = useLocation();
@@ -9,20 +14,23 @@ function Lyrics() {
     const { state: { songLyrics, artist, songTitle } = {}} = location;
     
     return (
-        <>
+        <div className="w-75 mx-auto">
             <Button href="/">Back</Button>
-            <h1>{ songTitle }</h1>
-            <h2>{ artist }</h2>
-            <div>
-                {
-                    songLyrics.map((line, index) =>
-                        <p key={index}>
-                            { line }
-                        </p>
-                    )
-                }
+            <div className="text-center">
+                <h1>"{ toTitleCase(songTitle) }"</h1>
+                <h2>by { toTitleCase(artist) }</h2>
+                <hr></hr>
+                <div>
+                    {
+                        songLyrics.map((line, index) =>
+                            <p key={index}>
+                                { line }
+                            </p>
+                        )
+                    }
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
