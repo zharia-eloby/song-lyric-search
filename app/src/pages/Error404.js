dee93e9b0c8426e37441f953d5fb74d10a9f7403
zharia-eloby <zeloby22@gmail.com>
2025-03-28 23:58:50 -0500

    updated error page

--- app/src/pages/Error404.js
+++ app/src/pages/Error404.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function Error404() {
     return (
-        <div id="error-page-message" className="text-center px-3 py-5 rounded rounded-5">
-            <h1>Page not found.</h1>
+        <div id="error-page-message" className="text-center px-3">
+            <h1>Page not found</h1>
             <Link className="fs-4" to="/">Go to homepage</Link>
         </div>
     );
