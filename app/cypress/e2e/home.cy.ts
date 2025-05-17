ddb2829e189feda2e3b215011bf211d991764d41
zharia-eloby <zeloby22@gmail.com>
2025-05-17 14:11:01 -0500

    added tests for api errors

--- app/cypress/e2e/home.cy.ts
+++ app/cypress/e2e/home.cy.ts
@@ -5,35 +5,58 @@ beforeEach(() => {
   cy.visit(home.url)
 })
 
-describe('When user searches for song lyrics', () => {
-  context('with valid input', () => {
-    it('directs to the lyrics page', () => {
-      cy.get(home.artistInputField).type('Etta James')
-      cy.get(home.songTitleInputField).type('At Last')
-      cy.get(home.submitButton).click()
-      cy.get(lyrics.lyrics).should('be.visible')
-      cy.get(lyrics.artistName)
-        .should('be.visible')
-        .contains('Etta James')
-      cy.get(lyrics.songTitle)
-        .should('be.visible')
-        .contains('At Last')
-    })
-  })
-
-  context('an error message is displayed', () => {
-    it('when submitting with empty artist input field', () => {
-      cy.get(home.songTitleInputField).type('At Last')
-      cy.get(home.submitButton).click()
-      cy.get(home.artistInvalidError).should('be.visible')
-      cy.get(home.songTitleInvalidError).should('not.be.visible')
+describe('Home Page', () => {
+  context('When user searches for song lyrics', () => {
+    context('with valid input', () => {
+      it('directs to the lyrics page', () => {
+        cy.get(home.artistInputField).type('Etta James')
+        cy.get(home.songTitleInputField).type('At Last')
+        cy.get(home.submitButton).click()
+        cy.get(lyrics.lyrics).should('be.visible')
+        cy.get(lyrics.artistName)
+          .should('be.visible')
+          .contains('Etta James')
+        cy.get(lyrics.songTitle)
+          .should('be.visible')
+          .contains('At Last')
+      })
     })
 
-    it('when submitting with empty song title input field', () => {
-      cy.get(home.artistInputField).type('Etta James')
-      cy.get(home.submitButton).click()
-      cy.get(home.artistInvalidError).should('not.be.visible')
-      cy.get(home.songTitleInvalidError).should('be.visible')
+    context('an error message is displayed', () => {
+      it('when submitting with empty artist input field', () => {
+        cy.get(home.songTitleInputField).type('At Last')
+        cy.get(home.submitButton).click()
+        cy.get(home.artistInvalidError).should('be.visible')
+        cy.get(home.songTitleInvalidError).should('not.be.visible')
+      })
+
+      it('when submitting with empty song title input field', () => {
+        cy.get(home.artistInputField).type('Etta James')
+        cy.get(home.submitButton).click()
+        cy.get(home.artistInvalidError).should('not.be.visible')
+        cy.get(home.songTitleInvalidError).should('be.visible')
+      })
+
+      it('when the song is not found', () => {
+        cy.get(home.artistInputField).type('somerandomartistinputfortesting')
+        cy.get(home.songTitleInputField).type('somerandomsongtitleinputfortesting')
+        cy.get(home.submitButton).click()
+        cy.get(home.notFoundError).should('be.visible')
+      })
+
+      it('when encountering an api error', () => {
+        cy.intercept('GET', 'https://api.lyrics.ovh/v1/**', { 
+          statusCode: 500,
+          body: { status: 500 }
+        }).as('apiErrorResponse')
+        
+        cy.get(home.artistInputField).type('Etta James')
+        cy.get(home.songTitleInputField).type('At Last')
+        cy.get(home.submitButton).click()
+        cy.wait('@apiErrorResponse')
+
+        cy.get(home.apiError).should('be.visible')
+      })
     })
   })
 })
