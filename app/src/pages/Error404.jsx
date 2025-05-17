6243da120c074ae5310d4cb1616fe9ef803c8200
zharia-eloby <zeloby22@gmail.com>
2025-05-17 17:55:26 -0500

    error page: resolved a11y violations + added data-testid

--- app/src/pages/Error404.jsx
+++ app/src/pages/Error404.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router";
 
 function Error404() {
   return (
-    <div id="error-page-message" className="text-center px-3">
+    <main role="main" id="error-page-message" className="text-center px-3" data-testid="error-404-page">
       <h1>Page not found</h1>
-      <Link className="fs-4" to="/">Go to homepage</Link>
-    </div>
+      <Link className="fs-4" to="/" data-testid="home-link">Go to homepage</Link>
+    </main>
   );
 }
 
